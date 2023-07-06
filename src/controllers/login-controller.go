04341e9a53fc872680f2eccc99bd1c1afa31cc72
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-07-06 18:23:29 -0300

    Login response changed

--- src/controllers/login-controller.go
+++ src/controllers/login-controller.go
@@ -3,9 +3,8 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
+	"web/src/models"
 	"web/src/responses"
 )
 
@@ -25,18 +24,23 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	response, err := http.Post("http://localhost:5000/login", "application/json",
 		bytes.NewBuffer(user))
 	if err != nil {
-		responses.JSON(w, http.StatusInternalServerError, responses.APIError{Error: err.Error()})
+		responses.JSON(w, http.StatusInternalServerError,
+			responses.APIError{Error: err.Error()})
 		return
 	}
-	//defer response.Body.Close()
+	defer response.Body.Close()
 
-	token, _ := io.ReadAll(response.Body)
-	fmt.Println(response.StatusCode, string(token))
+	if response.StatusCode >= 400 {
+		responses.CheckStatusCode(w, response)
+		return
+	}
 
-	// if response.StatusCode >= 400 {
-	// 	responses.CheckStatusCode(w, response)
-	// 	return
-	// }
+	var dataAuth models.DataAuth
+	if err = json.NewDecoder(response.Body).Decode(&dataAuth); err != nil {
+		responses.JSON(w, http.StatusUnprocessableEntity,
+			responses.APIError{Error: err.Error()})
+		return
+	}
 
-	// responses.JSON(w, response.StatusCode, nil)
+	responses.JSON(w, http.StatusOK, nil)
 }
