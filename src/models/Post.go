f829d8ef6e05c349962c2cfad894c5961b523775
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-07-17 21:34:12 -0300

    Post template and date format

--- src/models/Post.go
+++ src/models/Post.go
@@ -1,11 +1,13 @@
 package models
 
+import "time"
+
 type Post struct {
-	Id         uint64 `json:"id,omitempty"`
-	Title      string `json:"title,omitempty"`
-	Content    string `json:"content,omitempty"`
-	AuthorId   uint64 `json:"authorid,omitempty"`
-	AuthorName string `json:"authorname,omitempty"`
-	Likes      uint64 `json:"likes"`
-	RegDate    string `json:"regdate,omitempty"`
+	Id         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorId   uint64    `json:"authorid,omitempty"`
+	AuthorName string    `json:"authorname,omitempty"`
+	Likes      uint64    `json:"likes"`
+	RegDate    time.Time `json:"regdate,omitempty"`
 }
